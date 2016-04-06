f0d7e77328293c0c04de60a6f7f3a97b06a3e223
Beatrice Kirkbright <bzkirkbright@gmail.com>
2016-04-06 14:15:31 -0400

    P1 outline and wireframe

--- p1.js
+++ p1.js
@@ -1,8 +1,24 @@
 $(document).ready(function(){
 
-//BEACH CLEAN-UP 2 USERS 1 AT A TIME
+//BEACH CLEAN-UP MVP
 
-//
+//2 USERS - 1 AT A TIME - FASTEST TIME WINS
 
+//BEACH SET UP
+//--> CREATE LITTER LIST
+var litter = ['cigarette', 'six-pack', 'balloon' ]
+//--> GENERATE RANDOM LITTER
+//--> APPEND TO UI
+
+//PLAYER 1
+//--> PROMPT PLAYER HOW TO PLAY
+alert("Click the trash to clean up the beach!");
+//-->
+
+
+//BONUS
+//1. ASSIGN HIGHER VALUES FOR LARGER LITTER AND
+//2. LITTER BEACH WITH SAME TOTAL VALUE FOR EACH USER
+//3. TORE SCORES IN COOKIES
 
 });//CLOSES DOCUMENT READY
