9481f2e2ef781fb1cc7fe4cfbdeb6ce74565987a
Beatrice Kirkbright <bzkirkbright@gmail.com>
2016-04-07 13:29:27 -0400

    P1 Added litter to beach

--- js/p1.js
+++ js/p1.js
@@ -6,27 +6,75 @@ $(document).ready(function(){
 
 //BEACH SET UP
 //--> CREATE LITTER LIST
-var litter = ['cigarette', 'six-pack', 'balloon' ]
+var litter = [
+  ['cigarette', 'img/cigarette.png', 10],
+  ['sixpack', 'img/sixpack.png', 9],
+  ['balloon', 'img/balloon.png', 8],
+  ['trashbag', 'img/trashbag.png', 7],
+  ['logs', 'img/logs.png', 6],
+  ['fishNet', 'img/fishNet.png', 5],
+  ['waterbottle', 'img/waterbottle.png', 4],
+  ['balloon', 'img/cigarette.png', 3],
+  ['balloon', 'img/cigarette.png', 2],
+  ['balloon', 'img/cigarette.png', 1]
+];
 //--> GENERATE RANDOM LITTER
+var litterTheBeach = function(){
+
+  litter.forEach(function(){
+
+    //RANDOM LITTER SELECTION
+    var allTrash = litter[Math.floor(Math.random()*7)];
+    //SELECT ONLY THE IMAGES
+    var allTrashPics = allTrash[1];
+    console.log(allTrash[1]);
+    //CREATE DIV TO APPEND
+    var trash = $('<div class="trash"><img src=""/> </div>');
+    //ADD CONTAINERS TO BEACH
+    var beach = $('.beach');
+    beach.append(trash);
+
+    //LOOP THROUGH IMG AND APPEND EACH TO BEACH
+    for (var i = 0; i < allTrash.length; i++) {
+      //allTrashPics[i];
+
+      $('.trash img').attr('src', allTrashPics);
+
+    }
+
+
+    //RANDOM PLACEMENT OF TRASH ON BEACH
+    trash.css("top", Math.random() * beach.innerHeight);
+    trash.css("left", Math.random() * beach.innerWidth);
+
+
+     /*
+    $('.trash img').each(function(){
+          //Change the src of each img
+    });
+    */
+  })
+
+}
+litterTheBeach();
 //--> APPEND TO UI
 
 //PLAYER 1
-//--> 1. PROMPT PLAYER HOW TO PLAY
-//alert("Click the trash to clean up the beach! Fastest wins");
-//--> 2. CREATE PLAYER
-//--> 3. CREATE VAR SCORE = 0;
-//--> 4. CLICK EVENT TO 1) APPENDED TRASH & 2) SETTIMER
-//--> 5. ON CLICK REMOVE TRASH AND ADD POINTS TO SCORE
-//--> 6. WHEN ALL TRASH IS REMOVED 1) SETTIMEOUT 2) RETURN TOTAL SCORE
-//--> 7. STORE PLAYER 1 TIME
+//--> 1. CREATE PLAYER
+//--> 2. CREATE VAR SCORE = 0;
+//--> 3. CLICK EVENT TO 1) APPENDED TRASH & 2) SETTIMER
+//--> 4. ON CLICK REMOVE TRASH AND ADD POINTS TO SCORE
+//--> 5. WHEN ALL TRASH IS REMOVED 1) SETTIMEOUT 2) RETURN TOTAL SCORE
+//--> 6. STORE PLAYER 1 TIME
 
 
 //PLAYER 2
-//--> SAME AS 1-6
-//--> STORE PLAYER 2 TIME
+//--> SAME AS 1-5
+//--> 6. STORE PLAYER 2 TIME
 
 //COMPARE PLAYER 1 to PLAYER 2
 //--> Return winner
+//alert("Congratulations you saved a dolphin!")
 
 //BONUS
 //1. ASSIGN HIGHER VALUES FOR LARGER LITTER AND
